87bd2ecdda7c20fa2d996e494f34f9852b3985f5
OmarSebastian11 <143815501+OmarSebastian11@users.noreply.github.com>
2023-09-20 09:04:13 -0600

    Add files via upload

---
+++ Examen1Y2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen.pkg1.y.pkg2;
+import java.util.Scanner;
+/**
+ *
+ * @author Usuario
+ */
+public class Examen1Y2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+
+        System.out.println("dime el nombre del instrumento");
+        String nombre=entrada.next();
+        System.out.println("dime el precio de compra");
+        int precio=entrada.nextInt();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+    }
+    
+}
