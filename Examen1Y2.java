a7e3c583803ef74fe4e7b4d6aaf28913b97db02e
OmarSebastian11 <143815501+OmarSebastian11@users.noreply.github.com>
2023-09-20 11:35:23 -0600

    Update Examen1Y2.java

--- Examen1Y2.java
+++ Examen1Y2.java
@@ -15,12 +15,49 @@ public class Examen1Y2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double guitarra = 1250.45
+        double flauta = 3743.00
+        double bajo = 2683.78
+        
         Scanner entrada=new Scanner(System.in);
 
         System.out.println("dime el nombre del instrumento");
-        String nombre=entrada.next();
-        System.out.println("dime el precio de compra");
-        int precio=entrada.nextInt();
+        String nombre1=entrada.nextLine();
+        System.out.println("dime el nombre del instrumento");
+        String nombre2=entrada.nextLine();
+        System.out.println("dime el nombre del instrumento");
+        String nombre3=entrada.nextLine();
+
+        System.out.println("ingrese el precio de compra");
+        double precioCompra1 = entrada.nextDouble();
+        System.out.println("ingrese el precio de compra");
+        double precioCompra2 = entrada.nextDouble();
+        System.out.println("ingrese el precio de compra");
+        double precioCompra3 = entrada.nextDouble();
+
+        double ganancia1 = guitarra - precioCompra1;
+        double ganancia2 = flauta - precioCompra2;
+        double ganancia3 = bajo - precioCompra3;
+
+        double total1 = (guitarra/precioCompra1)*100;
+        double total2 = (flauta/precioCompra2)*100;
+        double total3 = (bajo/precioCompra3)*100;
+
+        System.out.println("ganancia de la guitarra" + ganancia1);
+        System.out.println("ganancia de la flauta" + ganancia2);
+        System.out.println("ganancia de bajo" + ganancia3);
+
+        System.out.println("total de la guitarra" + total1);
+        System.out.println("total de la flauta" + total2);
+        System.out.println("total del bajo" + total3);
+        
+        
+      
+
+        
+            
+        
+        
         
         
         
